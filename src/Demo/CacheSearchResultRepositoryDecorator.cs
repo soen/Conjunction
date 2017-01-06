4cb1732e49df4398dcdf3c63d697852c8414ea1a
Søren Engel <soren.engel@gmail.com>
2017-01-06 23:04:17 +0100

    Added example ISearchResultRepository with support for caching to the demo project

---
+++ src/Demo/CacheSearchResultRepositoryDecorator.cs
@@ -0,0 +1,30 @@
+﻿using Conjunction.Foundation.Core.Model;
+using Conjunction.Foundation.Core.Model.Repositories;
+using Sitecore.ContentSearch;
+
+namespace Demo
+{
+  /// <summary>
+  /// Example implementation of a custom decorator for giving search result repository implementations the option
+  /// of using caching.
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  public class CacheSearchResultRepositoryDecorator<T> : ISearchResultRepository<T> where T : IndexableEntity, new()
+  {
+    private readonly ISearchResultRepository<T> _searchResultRepository;
+
+    public CacheSearchResultRepositoryDecorator(ISearchResultRepository<T> searchResultRepository)
+    {
+      _searchResultRepository = searchResultRepository;
+    }
+
+    public ISearchIndex SearchIndex => _searchResultRepository.SearchIndex;
+
+    public SearchResult<T> GetSearchResult(SearchCriteria searchCriteria)
+    {
+      // TODO: Add caching...
+
+      return _searchResultRepository.GetSearchResult(searchCriteria);
+    }
+  }
+}
