af80bfc10733542b1c52d1ce5d68e3dbadb6669c
Søren Engel <sen@pentia.dk>
2017-08-06 20:29:54 +0200

    Fixed issue with trait discoverer on RequireLicenseAttribute

--- tests/Sitecore.Tests/RequireLicenseAttribute.cs
+++ tests/Sitecore.Tests/RequireLicenseAttribute.cs
@@ -4,7 +4,7 @@ using Xunit.Sdk;
 namespace Conjunction.Sitecore.Tests
 {
   // Adapted from: https://github.com/xunit/samples.xunit/tree/master/TraitExtensibility
-  [TraitDiscoverer("Conjunction.Foundation.Core.Tests.RequireLicenseDiscoverer", "Conjunction.Foundation.Core.Tests")]
+  [TraitDiscoverer("Conjunction.Sitecore.Tests.RequireLicenseDiscoverer", "Conjunction.Sitecore.Tests")]
   [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
   public class RequireLicenseAttribute : Attribute, ITraitAttribute
   {
